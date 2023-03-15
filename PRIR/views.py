afcd7c96452cbc74bbeea901deb1e6dfd106e143
mzaman21 <zwordpressservices@gmail.com>
2023-03-15 17:46:01 +0500

    Project setup

---
+++ PRIR/views.py
@@ -0,0 +1,16 @@
+# I have created this file
+#view aik http resonse send krta he
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def Index(request):
+    # return HttpResponse("<h1>PRIR</h1>")
+    params={'name':'zaman','place':'Lahore'}
+    return render(request, 'index.html',params)
+
+def brand_register(request):
+    return render(request,'brand_register.html')
+
+def brand_login(request):
+    return render(request,'brand_login.html')
